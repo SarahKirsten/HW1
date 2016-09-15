5b50ad4c7a5f942fb849827e734f51d8415664aa
SarahKirsten <deranged.link@gmail.com>
2016-09-15 14:05:23 -0700

    First commit of EX01_02

---
+++ EX01_02/EX01_02/Source.cpp
@@ -0,0 +1,26 @@
+/////////////////////////
+//Program Name: EX01_02
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 9/15/2016
+///////////////////////
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+
+
+int main()
+{
+
+	return 0;
+}
+
+
+
