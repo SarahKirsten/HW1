d35fb3a0481a2090378875cc60afcd8c9e19ce31
SarahKirsten <deranged.link@gmail.com>
2016-09-19 11:02:25 -0700

    First push of EX01_03. set up basic header and intructions for this part.

---
+++ EX01_03/EX01_03/Source.cpp
@@ -0,0 +1,42 @@
+/////////////////////////
+//Program Name: EX01_03
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 9/19/2016
+///////////////////////
+
+/*Write a function called ex04 takes no arguments and returns a void. call function in main
+in this function:
+
+write code to ask the user for a number between 1 and 10. Loop until the user gives a valid input
+
+use the number in part (a) to output the sum of the cubes from 1 to the number given (by cube, I mean that for a given number x, determinex3)
+
+use a do-while loop to output a number of asterisks, again using the input from (a)
+
+user a for loop to output the even numbers from 0 to 40
+
+implement a separate function that takes an integer, and doubles that integer. call that function from your ex04 function, using the value prompted in (a)
+
+write a function called add that takes two integers, and returns the sim of those integers. call it using two random numbers.
+
+write a function that adds one to its parameter. the function should take the interger as pass by reference.
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+
+	return 0;
+}
+
+
