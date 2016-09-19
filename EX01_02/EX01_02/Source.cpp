2b8e7ef47a53b90947b38f4a3bbaca38bfd8dd68
SarahKirsten <deranged.link@gmail.com>
2016-09-18 23:06:22 -0700

    EX01_02 worked on part a and b need a check on part c need help on part d

--- EX01_02/EX01_02/Source.cpp
+++ EX01_02/EX01_02/Source.cpp
@@ -5,6 +5,21 @@
 //Date: 9/15/2016
 ///////////////////////
 
+
+/* Write a function called ex03. returns a void. takes no arguments. call function in main().
+	in this function:
+
+	DONE	prompt user for area of square. output the diagonal for that square
+
+	DONE	prompt user for a yes or no response (y or n) using a char variable output "yes" if 'y' and "no" if 'n'
+
+	HELP/DONE	initialize a char variable tab to the tab character
+
+	HELP	declare a string variable mailingAddress, prompt user for their mailing address
+
+	initialize a string variable to the empty string
+
+*/
 #include <iostream>
 #include <iomanip>
 #include <ctime>
@@ -14,13 +29,38 @@
 #include <fstream>
 using namespace std;
 
-
+void ex03();//states a function
 
 int main()
 {
-
+	ex03();//calls the function
 	return 0;
 }
 
 
+void ex03()//the function: find diagonal of square. yes or no response. char variable tab. mailingAddress. string variable to empty string.
+{
+	int areaOfsquare;//initialize variable
+	cout << "Enter the area of a square: ";//prompt user
+	cin >> areaOfsquare;//input from user
+	cout << "The diagonal of this square is: " << sqrt(areaOfsquare + areaOfsquare) << endl << endl;//math to find diagonal length
 
+
+	char userInput;//char variable for yes/no response
+	cout << "Enter yes 'y' or no 'n': ";//prompt user
+	cin >> userInput;//input from user
+	if (userInput == 'y')//if loop to determine what to output
+		cout << "Yes.\n";
+	else if (userInput == 'n')
+		cout << "No.\n";
+	else
+		cout << "That isn't what I asked for...." << endl;//just a little fun if they want to go against the grain
+
+
+	char tab = '	';//tab variable with tab character
+
+
+	string mailingAddress;//string for mailing address input
+	cout << "\nEnter your mailing address: ";//prompt user for mailing address
+	getline (cin, mailingAddress); //input from user
+}
