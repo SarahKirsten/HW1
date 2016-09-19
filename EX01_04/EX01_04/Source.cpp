d2871e6db901de21b33268144accac5fe658eef2
SarahKirsten <deranged.link@gmail.com>
2016-09-19 11:06:40 -0700

    First push for EX01_04. includes basic header and instructions for this part

---
+++ EX01_04/EX01_04/Source.cpp
@@ -0,0 +1,37 @@
+/////////////////////////
+//Program Name: EX01_04
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 9/19/2016
+///////////////////////
+
+/*write a function called ex05, which takes no arguments and returns a void. call this function in main.
+in this function:
+
+write a loop that asks the user for five integers, and stores those integer values in an array
+
+write some code that calculates the sum and the product of the values in the integers, and outputs those results
+
+write a function that takes an array and the size of the array, and outputs the values in that array. 
+call the array from your ex05 function, passing the array of 5 integers from (a)
+
+write a function that takes an array and the size of that array, then prompts the user foa value, then outputs whether or not the array contains that value
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+
+	return 0;
+}
+
+
