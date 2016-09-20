c88a8cad06d4b2eb3e48c22fc89ef70c44eae115
SarahKirsten <deranged.link@gmail.com>
2016-09-19 22:17:50 -0700

    I need so much help on just review.

--- EX01_03/EX01_03/Source.cpp
+++ EX01_03/EX01_03/Source.cpp
@@ -8,9 +8,9 @@
 /*Write a function called ex04 takes no arguments and returns a void. call function in main
 in this function:
 
-write code to ask the user for a number between 1 and 10. Loop until the user gives a valid input
+DONE	write code to ask the user for a number between 1 and 10. Loop until the user gives a valid input
 
-use the number in part (a) to output the sum of the cubes from 1 to the number given (by cube, I mean that for a given number x, determinex3)
+HELP	use the number in part (a) to output the sum of the cubes from 1 to the number given (by cube, I mean that for a given number x, determine 3x)
 
 use a do-while loop to output a number of asterisks, again using the input from (a)
 
@@ -35,35 +35,49 @@ using namespace std;
 void ex04();//initialize function
 void separateFunction();//initialize function
 int add();//initialize function
-int &y;
-void parameter(int &y);//initialize function
+void parameter();//initialize function
 
 
 int main()
 {
-	ex04;//call function
+	ex04();//call function
 	return 0;
 }
 
 
 void ex04()//function: loop for number between 1 and 10. use that to output sum of cubes. asterisk. even numbers from 0 to 40. 
 {
+	int userInput;//variable for user input
+	do//do while loop to loop the prompt until the user complies
+	{
+
+		cout << "Enter a number between one and ten: ";
+		cin >> userInput;
+		if (userInput < 1 || userInput > 10)//to tell the user to try again because they won't listen to instructions
+			cout << "That isn't what I asked for.\n";
+
+	} while (userInput < 1 || userInput > 10);//keeps loop going until a number between (and including) 1 through 10 is entered
+
+	
+
+
 	separateFunction();//call function
-	add();//call function
-	parameter(y);//call function
+	cout << endl << add() << endl;//call function
+	parameter();//call function
 }
 
 void separateFunction()//function: takes integer, doubles integer using value in (a)
 {
-
+	cout << "\ntesting\n";
 }
 
-int add()//add two integersand returns the sum. using two random integers
+int add()//add two integers and returns the sum. using two random integers
 {
-
+	int sum = 5 + 32;
+	return sum;
 }
 
-void parameter(int &y)//add one to its parameter. should take the integer as pass by reference
+void parameter()//add one to its parameter. should take the integer as pass by reference
 {
-	y = y + 1;
+	cout << "\ntesting3\n\n";
 }
