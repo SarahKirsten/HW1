8bac1cd9e74cd6d155918f76a71f25361c552806
SarahKirsten <deranged.link@gmail.com>
2016-09-20 21:57:01 -0700

    FINISHED all parts of EX01_04

--- EX01_04/EX01_04/Source.cpp
+++ EX01_04/EX01_04/Source.cpp
@@ -15,7 +15,7 @@ DONE	write some code that calculates the sum and the product of the values in th
 DONE	write a function that takes an array and the size of the array, and outputs the values in that array.
 		call the array from your ex05 function, passing the array of 5 integers from (a)
 
-write a function that takes an array and the size of that array, then prompts the user for a value, then outputs whether or not the array contains that value
+DONE	write a function that takes an array and the size of that array, then prompts the user for a value, then outputs whether or not the array contains that value
 */
 
 #include <iostream>
@@ -29,7 +29,7 @@ using namespace std;
 
 void ex05();//initialize function
 void printArray(int arg[], int length);//initialize function
-void inMyarray();//initialize function
+void findInArray(int arg[], int length);//initialize function
 
 
 int main()
@@ -64,21 +64,33 @@ void ex05()//function: loop of five integers, stores values to an array. code th
 	}
 	cout << "The product of those numbers is: " << product << endl;//outputs the multiplying of the numbers in the array
 
+	
 
 	printArray(values, size);//call function
-	inMyarray();//call function
+	findInArray(values, size);//call function
+	cout << endl;
 }
 
 
 void printArray(int arg[], int length)//function: takes an array and size of that array and outputs the values in that array.
 {
 	cout << endl;
-	for (int n = 0;n < length;n++)
+	for (int n = 0; n < length; n++)
 		cout << arg[n] << " ";
 	cout << "\n\n";
 }
 
-void inMyarray()//function: takes array and size of that array. prompts user for value. outputs whether or not the array contains that value.
+void findInArray(int arg[], int length)//function: takes array and size of that array. prompts user for value. outputs whether or not the array contains that value.
 {
+	cout << endl << "Let's search for a number: ";
+	int findMe;
+	cin >> findMe;
+	for (int i = 0; i < length; i++)//loop to read in every number from user
+	{
+		if (findMe == arg[i])
+			cout << findMe << " exists in the array.\n";
+		else
+			cout << "Nope." << endl;
+	}
 
 }
