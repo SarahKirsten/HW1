32b82b7c5331047afae08f85436bd67e1a39a9ee
SarahKirsten <deranged.link@gmail.com>
2016-09-20 21:15:54 -0700

    EX01_04 Finished parts a, b, and c

--- EX01_04/EX01_04/Source.cpp
+++ EX01_04/EX01_04/Source.cpp
@@ -8,12 +8,12 @@
 /*write a function called ex05, which takes no arguments and returns a void. call this function in main.
 in this function:
 
-write a loop that asks the user for five integers, and stores those integer values in an array
+DONE	write a loop that asks the user for five integers, and stores those integer values in an array
 
-write some code that calculates the sum and the product of the values in the integers, and outputs those results
+DONE	write some code that calculates the sum and the product of the values in the integers, and outputs those results
 
-write a function that takes an array and the size of the array, and outputs the values in that array. 
-call the array from your ex05 function, passing the array of 5 integers from (a)
+DONE	write a function that takes an array and the size of the array, and outputs the values in that array.
+		call the array from your ex05 function, passing the array of 5 integers from (a)
 
 write a function that takes an array and the size of that array, then prompts the user for a value, then outputs whether or not the array contains that value
 */
@@ -28,7 +28,7 @@ write a function that takes an array and the size of that array, then prompts th
 using namespace std;
 
 void ex05();//initialize function
-void myArray();//initialize function
+void printArray(int arg[], int length);//initialize function
 void inMyarray();//initialize function
 
 
@@ -41,14 +41,41 @@ int main()
 
 void ex05()//function: loop of five integers, stores values to an array. code that calculates sum and product, outputs results. 
 {
-	myArray();//call function
+	int const size = 5;
+	int values[size];//states array with slots equal to size
+	cout << "Please enter 5 positive numbers.\n";//prompts for numbers
+	for (int i = 0; i < size; i++)//loop to read in every number from user
+	{
+		cin >> values[i];//stores to array
+	}
+	cout << endl;
+
+	int a;//count variable
+	int sum = 0;//initialize variable
+	int product = 1;//initialize variable
+	for (a = 0;a < 5;a++)//loop to add up all the numbers in the array
+	{
+		sum += values[a];
+	}
+	cout << "The sum of those numbers is: " << sum << endl;//outputs the sum of the numbers in the array
+	for (a = 0; a < 5; a++)//loop to multiply all the numbers in the array together
+	{
+		product *= values[a];
+	}
+	cout << "The product of those numbers is: " << product << endl;//outputs the multiplying of the numbers in the array
+
+
+	printArray(values, size);//call function
 	inMyarray();//call function
 }
 
 
-void myArray()//function: takes an array and size of that array and outputs the values in that array.
+void printArray(int arg[], int length)//function: takes an array and size of that array and outputs the values in that array.
 {
-	//call the array from ex05, passing the array of 5 integers from (a)
+	cout << endl;
+	for (int n = 0;n < length;n++)
+		cout << arg[n] << " ";
+	cout << "\n\n";
 }
 
 void inMyarray()//function: takes array and size of that array. prompts user for value. outputs whether or not the array contains that value.
